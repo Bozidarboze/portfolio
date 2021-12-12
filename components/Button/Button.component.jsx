d04db9936d5d39b8975324a01e60a35fc3dea5e0
Bozidarboze <Bozidar_boze123@hotmail.com>
2021-12-12 15:11:53 +0100

    .

--- components/Button/Button.component.jsx
+++ components/Button/Button.component.jsx
@@ -3,7 +3,7 @@ import styles from "./Button.module.scss";
 const Button = ({ href, label, target }) => {
   return (
     <div className={styles.button}>
-      <a target={target} href={href}>
+      <a target={target} href={href} rel='noreferrer'>
         {label}
       </a>
     </div>
--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -5,13 +5,13 @@ import styles from "./Footer.module.scss";
 const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.icon}>
-      <a href='https://github.com/bozidarboze' target='_blank'>
-        <Image src='/icons/github-icon.svg' width={50} height={50} />
+      <a href='https://github.com/bozidarboze' target='_blank' rel='noreferrer'>
+        <Image src='/icons/github-icon.svg' width={50} height={50} alt='github icon' />
       </a>
     </div>
     <div className={styles.icon}>
-      <a href='https://www.linkedin.com/in/bozidar-gjorgjievski-1a08a8219/' target='_blank'>
-        <Image src='/icons/linkedin-icon.svg' width={50} height={50} />
+      <a href='https://www.linkedin.com/in/bozidar-gjorgjievski-1a08a8219/' target='_blank' rel='noreferrer'>
+        <Image src='/icons/linkedin-icon.svg' width={50} height={50} alt='linkedin icon' />
       </a>
     </div>
   </footer>
