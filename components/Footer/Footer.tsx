1602ce8ed3d1720e198903f39272765ab4677d2f
Bozhidar Gjorgjievski <bozidar_boze123@hotmail.com>
2023-02-05 17:26:55 +0100

    Visual Improvements

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => (
   <footer className={styles.container}>
     <div className={styles.icons}>
       <Link href={'https://github.com/Bozidarboze'} target={'_blank'}>
-        <GitHub className={styles.icon} />
+        <GitHub sx={{ fontSize: '2rem' }} />
       </Link>
       <Link href={'https://www.linkedin.com/in/bozhidar-gjorgjievski-1a08a8219'} target={'_blank'}>
-        <LinkedIn className={styles.icon} />
+        <LinkedIn sx={{ fontSize: '2rem' }} />
       </Link>
     </div>
     <span>Bozidar_boze123@hotmail.com</span>
--- components/HeroView/HeroView.tsx
+++ components/HeroView/HeroView.tsx
@@ -1,7 +1,6 @@
 import styles from './HeroView.module.css';
 import Image from 'next/image';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { DeveloperMode } from '@mui/icons-material';
 
 const HeroView = () => (
   <section className={styles.container}>
@@ -9,29 +8,29 @@ const HeroView = () => (
       <span>Hello, my name is</span>
       <h1>Bozhidar Gjorgjievski</h1>
       <p>
-        I have experience in developing, designing and deploying complete web applications. I understand
-        the entire web development process, from the initial concept to deployment, and have a good
-        understanding of the architectural patterns and practices used in web development.
+        I have experience in developing, designing and deploying complete web applications. I
+        understand the entire web development process, from the initial concept to deployment, and
+        have a good understanding of the architectural patterns and practices used in web
+        development.
       </p>
     </div>
     <Image
-        className={styles.graphic1}
-        src={'./graphic1.svg'}
-        alt={'graphic'}
-        width={100}
-        height={100}
+      className={styles.graphic1}
+      src={'./graphic1.svg'}
+      alt={'graphic'}
+      width={100}
+      height={100}
     />
     <Image
-        className={styles.graphic2}
-        src={'./graphic1.svg'}
-        alt={'graphic'}
-        width={100}
-        height={100}
+      className={styles.graphic2}
+      src={'./graphic1.svg'}
+      alt={'graphic'}
+      width={100}
+      height={100}
     />
     <div className={styles.arrowDown}>
       <KeyboardArrowDownIcon fontSize={'large'} />
     </div>
-    <DeveloperMode className={styles.icon} />
   </section>
 );
 
